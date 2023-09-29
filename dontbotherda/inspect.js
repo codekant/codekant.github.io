17c09bbca81e8b667c1562e309354dc51800fab1
カント <53932812+itsjustkant@users.noreply.github.com>
2023-09-29 23:05:11 +0530

    sdfdsf

--- dontbotherda/inspect.js
+++ dontbotherda/inspect.js
@@ -4,6 +4,12 @@ window.onload = function() {
             mouseControls: true,
             touchControls: true,
             gyroControls: true,
+            skyColor: 0xd685e,
+            cloudColor: 0x1d3447,
+            cloudShadowColor: 0x0,
+            sunColor: 0x446a4c,
+            sunGlareColor: 0xae7d6c,
+            sunlightColor: 0xc78a8a,
             speed: 1.5
     })
 }
