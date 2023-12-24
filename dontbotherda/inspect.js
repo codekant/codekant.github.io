81e74ab642fb2b77493a0e1cfa051f9141e79b39
カント <53932812+itsjustkant@users.noreply.github.com>
2023-12-24 17:39:14 +0530

    in

--- dontbotherda/inspect.js
+++ dontbotherda/inspect.js
@@ -1,3 +1,4 @@
+let date = "22 December, 2023"
 window.onload = function() {
     VANTA.CLOUDS({
             el: ".fullscreen",
@@ -11,4 +12,22 @@ window.onload = function() {
             sunlightColor: 0xe3849d,
             speed: 1.00
     }) 
+    function update(date) {
+        const d = new Date(date);
+        let timestamp = new Date(Date.now() - d);
+        const glue = {
+            days: Math.floor((Date.now() - d)/84000000),
+            hours: timestamp.getUTCHours(),
+            mins: timestamp.getUTCMinutes(),
+            secs: timestamp.getUTCSeconds()
+        }
+        let final = (glue.days < 10 ? "0" + glue.days : glue.days) + ":" + 
+        (glue.hours < 10 ? "0" + glue.hours : glue.hours) + ":" +
+        (glue.mins < 10 ? "0" + glue.mins : glue.mins) + ":" + 
+        (glue.secs < 10 ? "0" + glue.secs : glue.secs);
+        setTimeout(() => {
+            $("#sheleftmelol").html(final);
+        }, 1000)
+    }
+    setInterval(() => {update(date)}, 1000)
 }
